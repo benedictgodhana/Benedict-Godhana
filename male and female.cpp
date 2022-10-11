f079be3d132cc83fe037c220f2b6588f33b3b24f
j3rryl <jeremymunroe0@gmail.com>
2022-10-11 14:14:05 +0300

    ...

---
+++ male and female.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"\t    O\t    O\n";
+    cout <<"\t  --|--\t  --|--\n";
+    cout <<"\t    |\t   /_\\ \n";
+    cout <<"\t   / \\\t  /   \\ \n";
+    cout <<"\t\t";
+}
